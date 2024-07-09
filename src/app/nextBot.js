30e7a09ed1064ecf01c742e0abbd3adfcd48fd39
DS-CodeCrunch <dhruv@DS-CodeCrunchs-MacBook-Air.local>
2024-07-09 14:20:38 +0530

    first commit

---
+++ src/app/nextBot.js
@@ -0,0 +1,18 @@
+const { Telegraf } = require("telegraf");
+require('dotenv')
+
+const TOKEN = process.env.NEXT_PUBLIC_BOT_TOKEN;
+// console.log(TOKEN,'--------------');
+const bot = new Telegraf('7313216675:AAENDcBucJ2Pjltw9M7IMV_E2IjhfUr-8Cg');
+
+const web_link = "https://sfs-two.vercel.app/superplay";
+
+bot.start((ctx) =>
+    ctx.reply("Welcome To the SFS :) ", {
+        reply_markup: {
+            keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
+        },
+    })
+);
+
+bot.launch();
