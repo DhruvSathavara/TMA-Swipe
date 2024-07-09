fc2fbcafde1877cd5783439d2dae7a691e9047b7
DS-CodeCrunch <dhruv@DS-CodeCrunchs-MacBook-Air.local>
2024-07-09 14:23:39 +0530

    change-live-url

--- src/app/nextBot.js
+++ src/app/nextBot.js
@@ -5,7 +5,7 @@ const TOKEN = process.env.NEXT_PUBLIC_BOT_TOKEN;
 // console.log(TOKEN,'--------------');
 const bot = new Telegraf('7313216675:AAENDcBucJ2Pjltw9M7IMV_E2IjhfUr-8Cg');
 
-const web_link = "https://sfs-two.vercel.app/superplay";
+const web_link = "https://tma-swipe.vercel.app/";
 
 bot.start((ctx) =>
     ctx.reply("Welcome To the SFS :) ", {
